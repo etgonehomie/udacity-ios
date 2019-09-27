34cb5be3f4c4a0085b9f7a61c47728170885d19e
eric-tom <emtom22@gmail.com>
2019-09-27 10:56:47 -0700

    boiler plate for testing

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Eric Tom on 9/27/19.
+//  Copyright © 2019 Eric Tom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
